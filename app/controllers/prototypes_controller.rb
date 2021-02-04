8b145de46dea61ae6f96761a85a097c5048adb55
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-02-04 20:16:54 +0900

    gem2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -44,6 +44,7 @@ class PrototypesController < ApplicationController
     end
   end
 
+  
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
